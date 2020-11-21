cffcdc8418ae2a55842cd86b7408a3870d51634f
koushikdev1 <koushik@billionlives.in>
2020-11-21 18:23:42 +0530

    stock availability option working properly

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ var app = new Vue({
     data: {
         brand: "Vue mastery",
         product: "socks",
-        image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
-        inStock: false,
+        selectedVariant: 0,
         details: ["80% cotton","10% polyster","10% silk"],
         variants: [
             {
                 variantId: 1111,
                 variantColor: "green",
-                variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+                variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2222,
                 variantColor: "blue",
-                variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg"
+                variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
 
             }
         ],
@@ -29,13 +30,20 @@ var app = new Vue({
         remove(){
             this.cart = "0"
         },
-        updateProduct(variantImage){
-            this.image = variantImage
+        updateProduct(index){
+            this.selectedVariant = index;
+            
         }
     },
     computed: {
         title(){
             return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
